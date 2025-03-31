f3e978779de2ceff43714951b5dd70639088c1c8
Atul yadav <57094600+atul2501@users.noreply.github.com>
2025-03-31 19:11:46 +0530

    Create tmdb.py

---
+++ tmdb.py
@@ -0,0 +1,31 @@
+api_key='c38986c04eeba139e93aae724f375443'
+
+final=pd.DataFrame()
+for j in range(1,501):
+    response=requests.get(f'https://api.themoviedb.org/3/movie/popular?api_key={api_key}&language=en-US&page={format(j)}').json()
+
+    popularity=[]
+    vote_count=[]
+    original_language=[]
+    title=[]
+    vote_average=[]
+    release_date=[]
+    overview=[]
+    
+    
+    
+    for i in response['results']:
+        title.append(i['title'])
+        popularity.append(i['popularity'])
+        vote_count.append(i['vote_count'])
+        original_language.append(i['original_language'])
+        vote_average.append(i['vote_average'])
+        release_date.append(i['release_date'])
+        overview.append(i['overview'])
+    
+    d={'title':title,'overview':overview,'popularity':popularity,'vote_count':vote_count,'original_language':original_language,'vote_average':vote_average,'release_date':release_date}
+    
+    df=pd.DataFrame(d)
+    
+    #final=final.append(df, ignore_index=True) # error reson is append and concat to 
+    final = pd.concat([final, df], ignore_index=True)
